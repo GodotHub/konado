9c8ce3feea0e509c2d92cf341cf6277d11d58877
DSOE1024 <computerlub@163.com>
2025-08-24 19:11:14 +0800

    更新Konado脚本导入器导入选项功能

--- addons/konado/importer/konado_importer.gd
+++ addons/konado/importer/konado_importer.gd
@@ -11,10 +11,10 @@ func _get_priority():
 	return 1.0
 
 func _get_visible_name():
-	return "Konado Scripts 可娜多脚本"
+	return "Konado Scripts"
 
 func _get_recognized_extensions():
-	return ["ks"]
+	return ["ks", "txt"]
 
 func _get_save_extension():
 	return "res"
@@ -29,7 +29,16 @@ func _get_preset_name(preset_index):
 	return "Default"
 
 func _get_import_options(path, preset_index):
-	return []
+	return [
+		{
+			"name": "allow_skip_error_line",
+			"default_value": false
+		},
+		{
+			"name": "enable_actor_validation",
+			"default_value": true
+		}
+		]
 
 func _get_option_visibility(path, option_name, options):
 	return true
@@ -39,7 +48,8 @@ func _import(source_file, save_path, options, platform_variants, gen_files):
 
 	interpreter.init_insterpreter({
 		"allow_custom_suffix": true,
-		"enable_actor_validation": true
+		"allow_skip_error_line": options["allow_skip_error_line"],
+		"enable_actor_validation": options["enable_actor_validation"]
 	})
 	
 	var diadata = interpreter.process_scripts_to_data(source_file)
@@ -47,7 +57,7 @@ func _import(source_file, save_path, options, platform_variants, gen_files):
 		printerr("Failed to process scripts")
 		return FAILED
 	var output_path = "%s.%s" % [save_path, _get_save_extension()]
-	var error = ResourceSaver.save(diadata, output_path, 
+	var error = ResourceSaver.save(diadata, output_path,
 		ResourceSaver.FLAG_COMPRESS | ResourceSaver.FLAG_REPLACE_SUBRESOURCE_PATHS)
 	
 	if error != OK:
