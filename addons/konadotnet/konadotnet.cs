7696e2d1aaf3d3b26ba9d9da127eb0b2ff4f8b6b
DSOE1024 <computerlub@163.com>
2025-08-26 18:25:55 +0800

    实现插件依赖检查

--- addons/konadotnet/konadotnet.cs
+++ addons/konadotnet/konadotnet.cs
@@ -3,22 +3,58 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
+/// <summary>
+/// Konadotnet插件入口类
+/// </summary>
 [Tool]
 public partial class konadotnet : EditorPlugin
 {
+	/// <summary>
+	/// 插件自动加载的类，键为类名，值为类路径
+	/// </summary>
 	private Dictionary<string, string> _autoloads = new Dictionary<string, string>()
 	{
 		{"DialogueManagerAPI", "res://addons/konadotnet/api/DialogueManagerAPI.cs"}
 	};
-	
+
+	private bool _isCommunityEdition = true;
+
 	public override void _EnterTree()
 	{
-		GD.Print("Hello from Konadotnet!");
+		GD.Print("Konadotnet插件加载中...");
+		// 检查插件路径
+		if (FileAccess.FileExists("res://addons/konado/plugin.cfg") == false)
+		{
+			GD.PrintErr("Konado插件未安装，无法加载Konadotnet插件");
+			return;
+		}
+
+		Godot.Collections.Array<string> pluginConfig = ProjectSettings.GetSetting("editor_plugins/enabled").As<Godot.Collections.Array<string>>();
+
+		if (pluginConfig != null)
+		{
+			if (pluginConfig.Contains("res://addons/konado/plugin.cfg"))
+			{
+				GD.Print("检查Konado插件已启用");
+			}
+			else
+			{
+				GD.PrintErr("Konado插件未启用，请先在项目设置启用Konado插件");
+				return;
+			}
+		}
+		else
+		{
+			GD.PrintErr("无法获取项目插件配置信息，请检查project.godot文件");
+			return;
+		}
 
 		foreach (var autoload in _autoloads)
 		{
 			AddAutoloadSingleton(autoload.Key, autoload.Value);
 		}
+
+		GD.Print("Konadotnet插件加载成功");
 
 
 	}
