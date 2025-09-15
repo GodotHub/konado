56e150f43a1852f4c162f07c8c363dd7b8b87dea
DSOE1024 <computerlub@163.com>
2025-09-16 05:49:18 +0800

    更新数据库脚本

--- addons/konado/database/db_test.gd
+++ addons/konado/database/db_test.gd
@@ -2,7 +2,16 @@ extends Node
 
 
 func _ready() -> void:
-	#for i in 100:
-		#KND_Database.create_data("KND_Shot")
-	for i in 10000:
-		KND_Database.create_data("KND_Character")
+	KND_Database.load_database()
+		
+
+	for k in 5:
+		var id = KND_Database.create_data("KND_Shot")
+		var sub = KND_Database.create_sub_data("KND_Dialogue")
+		KND_Database.add_sub_source_data(id, sub["id"], sub["data"])
+		
+	await get_tree().create_timer(0.1).timeout
+	KND_Database.save_database()
+	
+
+	
