dbccdd3cc00cea09a075c1c4aa76a81b158c3ae2
DSOE1024 <computerlub@163.com>
2025-10-05 13:00:55 +0800

    feat(*): 更新knd数据类，补充部分函数和注释

--- addons/konado/knd_data/act/knd_background.gd
+++ addons/konado/knd_data/act/knd_background.gd
@@ -2,9 +2,15 @@
 extends KND_Data
 class_name KND_Background
 
-const icon = preload("uid://1b3bn1nwu3x")
+const icon: Texture2D = preload("uid://1b3bn1nwu3x")
 ## 背景名称
 @export var name: String = "新背景"
 
 ## 背景图片
 @export var background_image_path: String
+
+
+## 加载背景图片纹理
+func get_background_texture() -> Texture2D:
+	var texture: Texture2D = ResourceLoader.load(background_image_path)
+	return texture
--- addons/konado/knd_data/audio/knd_sfx.gd
+++ addons/konado/knd_data/audio/knd_sfx.gd
@@ -1,3 +1,5 @@
 @tool
 extends KND_Data
 class_name KND_SFX
+
+var name: String = "音效"
